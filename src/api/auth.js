917eefbd48af97ebdfdaa4a63bb5530b75c79e50
Hyojin-Moon <moontough8@gmail.com>
2025-02-25 13:01:30 +0900

    ✏️ Fix: 회원정보수정 버그 fix / instance사용전으로 롤백

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,29 +1,36 @@
-import api from './api';
+import axios from 'axios';
+
+const API_URL = import.meta.env.VITE_USER_API_URL;
 
 export const register = async (userData) => {
-  const response = await api.post("/register", userData);
+  const response = await axios.post(`${API_URL}/register`, userData);
   return response.data;
 };
 
 export const login = async (userData) => {
-  const response = await api.post("/login", userData);
+  const response = await axios.post(`${API_URL}/login`, userData);
   return response.data;
 };
 
-export const getUserProfile = async () => {
-  const response = await api.get("/user");
+export const getUserProfile = async (token) => {
+  const response = await axios.get(`${API_URL}/user`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    }
+  });
   return response.data;
 };
 
-export const updateProfile = async (nickname, avatar) => {
+export const updateProfile = async (nickname, avatar, token) => {
 
   const formData = new FormData();
   formData.append("nickname", nickname);
   if (avatar) formData.append("avatar", avatar);
 
-  const response = await api.patch("/profile", formData, {
+  const response = await axios.patch(`${API_URL}/profile`, formData, {
     headers: {
       "Content-Type": "multipart/form-data",
+      Authorization: `Bearer ${token}`,
     },
   });
   return response.data;
